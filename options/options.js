4f7d91b862828b9016056c08cac8f38829cad6c5
Crowdbotics <team@crowdbotics.com>
2023-05-15 19:27:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kyblibre-40713",
+  url: "https://kyblibre-40713.botics.co",
+  api: "https://kyblibre-40713.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
